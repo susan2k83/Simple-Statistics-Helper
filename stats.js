167bc9eb2152835c95f0ac4a0c22e3ee97a0acc7
Susan <susan2k83@yahoo.com>
2018-11-08 23:15:00 -0800

    updated MODE code and added a page reload btn

--- stats.js
+++ stats.js
@@ -4,9 +4,8 @@ $(document).ready(function () {
 
 $("#sortBtn").on("click", function(){
     var input =[];
-    
     input.push($("#numberInputBox").val());
-       
+       //--code below: original user input returns as one object. The object needs to be converted to a string, then split, then coverted to a number. THEN the .sort() method is used to arrange the numbers in numerical order.
    var newArr = input.toString().split(" ").map(Number);
   
    var numSorted = newArr.sort(function(a, b){
@@ -15,21 +14,24 @@ $("#sortBtn").on("click", function(){
      $.each(numSorted, function(index, value){
         $('#numberSortedBox').append( value + "," + " ");
     });
+    //--math.js is used to find the MEAN
     var meanStat = math.mean(numSorted);
     meanStat = meanStat.toFixed(4);
-    console.log(meanStat);
     $('#meanBox').html(meanStat);
 
-    var modeStat = math.mode(numSorted);
-    console.log(modeStat);
-    $('#modeBox').html(modeStat);
-
+    // did not use math.js for MODE because it would not isolate the mode, rather it would return all unique numbers
+    var duplicates = numSorted.filter(i => numSorted.filter(ii => ii === i).length > 1);
+    $.each(duplicates, function(index,value){
+        $('#modeBox').append(value + " " + " ");
+    });
+    //--math.js is used to find the MEDIAN---
     var medianStat = math.median(numSorted);
-    console.log(medianStat);
     $('#medianBox').html(medianStat);
-
-
+    
   });
+  $('#reloadBtn').on("click", function(){
+      location.reload();
+  })
 
 });
 
